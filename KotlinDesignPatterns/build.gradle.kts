851037bda93940d82d61168b8897a77e2da632d3
ancuta <ancuta.maria@gmail.com>
2022-07-21 21:07:54 +0300

    configure unit tests

--- KotlinDesignPatterns/build.gradle.kts
+++ KotlinDesignPatterns/build.gradle.kts
@@ -13,6 +13,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.23.1")
 }
 
 tasks.test {
